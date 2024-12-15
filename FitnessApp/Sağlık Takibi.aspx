c866074d2a368e56d053a9aef2df08f71561226c
AlperenJosephson <108187147+AlperenJosephson@users.noreply.github.com>
2024-12-15 17:46:01 +0300

    Sağlık Takibi Ekranı Yapıldı. Veriler başarılı bir şekilde databaseye kaydediliyor ve çekiliyor. Farklı hesaplar arasında veri kaybı veya karmaşası oluşmuyor.

--- FitnessApp/Sağlık Takibi.aspx
+++ FitnessApp/Sağlık Takibi.aspx
@@ -6,4 +6,63 @@
 </asp:Content>
 
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <div class="container mt-4">
+        <!-- Veri Giriş Alanları -->
+        <div class="row mb-4">
+            <div class="col-md-3">
+                <label>Kilo (kg):</label>
+                <asp:TextBox ID="txtKilo" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="col-md-3">
+                <label>Boy (cm):</label>
+                <asp:TextBox ID="txtBoy" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="col-md-3">
+                <label>Adım Sayısı:</label>
+                <asp:TextBox ID="txtAdimSayisi" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="col-md-3">
+                <label>Egzersiz Adı:</label>
+                <asp:TextBox ID="txtEgzersizAdi" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col-md-3">
+                <label>Egzersiz Süresi (dk):</label>
+                <asp:TextBox ID="txtEgzersizSuresi" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="col-md-3 mt-4">
+                <asp:Button ID="btnKaydet" runat="server" Text="Kaydet" CssClass="btn btn-primary" OnClick="btnKaydet_Click" />
+            </div>
+        </div>
+
+        <!-- Sağlık Verilerinin Listelenmesi -->
+        <h4>Sağlık Verileriniz</h4>
+        <asp:Repeater ID="RepeaterSaglikTakibi" runat="server">
+            <HeaderTemplate>
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Tarih</th>
+                        <th>Kilo (kg)</th>
+                        <th>Boy (cm)</th>
+                        <th>Adım Sayısı</th>
+                        <th>Egzersiz Adı</th>
+                        <th>Süre (dk)</th>
+                    </tr>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    <td><%# Eval("Tarih") %></td>
+                    <td><%# Eval("Kilo") %></td>
+                    <td><%# Eval("Boy") %></td>
+                    <td><%# Eval("AdimSayisi") %></td>
+                    <td><%# Eval("EgzersizAdi") %></td>
+                    <td><%# Eval("EgzersizSuresi") %></td>
+                </tr>
+            </ItemTemplate>
+            <FooterTemplate>
+                </table>
+            </FooterTemplate>
+        </asp:Repeater>
+    </div>
 </asp:Content>
--- FitnessApp/Sağlık Takibi.aspx.cs
+++ FitnessApp/Sağlık Takibi.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+
+
 
 namespace FitnessApp
 {
@@ -11,7 +14,101 @@ namespace FitnessApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadHealthData();
+            }
+        }
 
+        string connectionString = "Data Source=localhost\\SQLEXPRESS01;Initial Catalog=Deneme_Login;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
+        protected void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string userMail = Session["Kullanici"] as string;
+            if (string.IsNullOrEmpty(userMail))
+            {
+                Response.Redirect("~/Login.aspx");
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"INSERT INTO SaglikTakibi (Mail, Tarih, Kilo, Boy, AdimSayisi, EgzersizAdi, EgzersizSuresi) 
+                                 VALUES (@Mail, @Tarih, @Kilo, @Boy, @AdimSayisi, @EgzersizAdi, @EgzersizSuresi)";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Mail", userMail);
+                command.Parameters.AddWithValue("@Tarih", DateTime.Now);
+                command.Parameters.AddWithValue("@Kilo", txtKilo.Text);
+                command.Parameters.AddWithValue("@Boy", txtBoy.Text);
+                command.Parameters.AddWithValue("@AdimSayisi", txtAdimSayisi.Text);
+                command.Parameters.AddWithValue("@EgzersizAdi", txtEgzersizAdi.Text);
+                command.Parameters.AddWithValue("@EgzersizSuresi", txtEgzersizSuresi.Text);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+            // Veriyi kaydettikten sonra listeyi yeniden yükle
+            LoadHealthData();
+            ClearFields();
+        }
+
+
+        private void LoadHealthData()
+        {
+            string userMail = Session["Kullanici"] as string;
+            if (string.IsNullOrEmpty(userMail)) return;
+
+            List<HealthData> healthDataList = new List<HealthData>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Tarih, Kilo, Boy, AdimSayisi, EgzersizAdi, EgzersizSuresi FROM SaglikTakibi WHERE Mail = @Mail";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Mail", userMail);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    healthDataList.Add(new HealthData
+                    {
+                        Tarih = Convert.ToDateTime(reader["Tarih"]).ToString("dd/MM/yyyy"),
+                        Kilo = reader["Kilo"].ToString(),
+                        Boy = reader["Boy"].ToString(),
+                        AdimSayisi = reader["AdimSayisi"].ToString(),
+                        EgzersizAdi = reader["EgzersizAdi"].ToString(),
+                        EgzersizSuresi = reader["EgzersizSuresi"].ToString()
+                    });
+                }
+                reader.Close();
+            }
+
+            RepeaterSaglikTakibi.DataSource = healthDataList;
+            RepeaterSaglikTakibi.DataBind();
+        }
+
+        private void ClearFields()
+        {
+            txtKilo.Text = "";
+            txtBoy.Text = "";
+            txtAdimSayisi.Text = "";
+            txtEgzersizAdi.Text = "";
+            txtEgzersizSuresi.Text = "";
+        }
+
+        public class HealthData
+        {
+            public string Tarih { get; set; }
+            public string Kilo { get; set; }
+            public string Boy { get; set; }
+            public string AdimSayisi { get; set; }
+            public string EgzersizAdi { get; set; }
+            public string EgzersizSuresi { get; set; }
         }
     }
 }
